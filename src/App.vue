aaa06ff0dfd97380bf907be8e4e8dfc96c693d42
brandoncasasnovas <haphazerdous@aol.com>
2019-08-27 22:29:32 -0400

    calling todo app v 1.0.0, still needs commenting throughout application

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,16 @@
- <template>
+<template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-   <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/Layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
+import Header from './components/Layout/Header'
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Header,
-    Todos,
-    AddTodo
-  },
-  data(){
-    return{
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: false
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
-      ]
-    }
-  },
-  methods:{
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    },
-    addTodo(newTodo){
-      this.todos = [...this.todos, newTodo];
-    }
   }
 }
 </script>
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the todo list version 1.0.0, it is part of the Vue course.</p>
+  </div>
+</template>
+
+<style scoped>
+  h1{
+    padding-top: 2rem;
+    text-align: center;
+  }
+  p {
+    text-align: center;
+  }
+</style>
